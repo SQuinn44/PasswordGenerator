d9e146d63cac9567b9ce2006441820aed2318634
SQuinn44 <65035946+SQuinn44@users.noreply.github.com>
2020-06-25 19:21:49 -0500

    Added clipboard event listener

--- main.js
+++ main.js
@@ -14,3 +14,17 @@ const randomFunc = {
     symbol: getRandomSymbol
  }
 
+ clipboard.addEventListener('click', () => {
+    const textarea = document.createElement('textarea');
+    const password = resultEl.innerText;
+   
+    if(!password) { return; }
+   
+    textarea.value = password;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+    alert('Password copied to clipboard');
+ });
+ 
